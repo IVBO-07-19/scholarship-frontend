257eb6cc4b875adf5e3e408038cd3cbef1e3d79f
Ekaterina Gibova <katyagibova@bk.ru>
2021-04-15 17:42:26 +0300

    Refactoring app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 // import CommentComponent from './components/CommentComponent';
 // import PostComponent from './components/PostComponent';
@@ -15,28 +14,10 @@ import Documents from './components/Documents/Documents';
 import Main from './components/Main/Main';
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <SportConnectionComponent />
-    //     <CultureConnectionComponent />
-    //     <EduConnectionComponent />
-    //     <ScienceConnectionComponent />
-    //
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-          <Header></Header>
+          <Header />
           <div>
             <Route path='/Main' render={()=> <Main />} />
             <Route path='/Information' render={()=> <Information />} />
