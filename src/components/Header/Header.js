18c6666ce9eae6a3b7876ed61378be32b5c35625
Netlighter <jasoness@yandex.ru>
2021-04-29 17:51:54 +0300

    Fix for merge #2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,29 +6,7 @@ import LogOutBtn from '../LogButtons/LogOutBtn';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Header = () => {
-<<<<<<< Updated upstream
-    const { isAuthenticated } = useAuth0();
-
-        return(
-            <header className={s.headerbar}>
-                        <NavLink to="/" className={s.nav1}>УВиСР</NavLink>
-                        <span></span>
-                        <NavLink to="/Information" className={s.nav2}>Информация</NavLink>
-                        <span></span>
-                        <NavLink to="/Documents" className={s.nav2}>Необходимые документы</NavLink>
-                        <span></span>
-                <div></div>
-                <div className={s.btns}>
-                    {!isAuthenticated ?(
-                        <LogInBtn></LogInBtn>
-                        ):(
-                        <LogOutBtn></LogOutBtn>
-                        )}
-                </div>
-=======
   const { isAuthenticated } = useAuth0();
->>>>>>> Stashed changes
-
   return (
     <header className={s.headerbar}>
       <NavLink to="/scholarship-frontend" className={s.nav1}>
