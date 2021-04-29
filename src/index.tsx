3b83c9a71f6405c0db1f5963a760ded30fc2c078
Netlighter <jasoness@yandex.ru>
2021-04-29 17:30:47 +0300

    Little refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js';
-import { Auth0Provider } from '@auth0/auth0-react'
+import App from './App.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
 //import reportWebVitals from './reportWebVitals';
 
 // const domain = process.env.REACT_APP_AUTH0_DOMAIN ?? "";
 // const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? "";
 
 ReactDOM.render(
-  <Auth0Provider domain = "dev-pmfwjryi.eu.auth0.com" clientId = "VSSSQPMROlOksW0DoYFN7NjoIwy0HxGi"  redirectUri={window.location.origin}>
-
+  <Auth0Provider
+    domain="dev-pmfwjryi.eu.auth0.com"
+    clientId="VSSSQPMROlOksW0DoYFN7NjoIwy0HxGi"
+    redirectUri={window.location.origin}
+  >
     <App />
   </Auth0Provider>,
   document.getElementById('root')
