8f6333a47cee8351e808422a06ca481ecd1f70e2
Netlighter <jasoness@yandex.ru>
2021-05-05 17:33:27 +0300

    Fix index and env.local

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
-//import reportWebVitals from './reportWebVitals';
 
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN ?? "";
-// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? "";
+const domain = process.env.REACT_APP_AUTH0_DOMAIN ?? '';
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? '';
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-pmfwjryi.eu.auth0.com"
-    clientId="VSSSQPMROlOksW0DoYFN7NjoIwy0HxGi"
+    domain={domain}
+    clientId={clientId}
     redirectUri={window.location.origin + '/scholarship-frontend'}
   >
     <App />
   </Auth0Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
